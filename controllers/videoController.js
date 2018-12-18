96ca75d8dfa4a46c72a30dcbff0b40237f293912
WHYjun <yjunchoi@bu.edu>
2018-12-18 12:41:15 -0500

    set up views directory for pug

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,6 +1,6 @@
-export const home = (req, res) => res.send("");
-export const search = (req, res) => res.send("");
-export const videoDetail = (req, res) => res.send("");
-export const uploadVideo = (req, res) => res.send("");
-export const editVideo = (req, res) => res.send("");
-export const deleteVideo = (req, res) => res.send("");
+export const home = (req, res) => res.render("home");
+export const search = (req, res) => res.render("search");
+export const videoDetail = (req, res) => res.render("videoDetail");
+export const uploadVideo = (req, res) => res.render("uplaodVideo");
+export const editVideo = (req, res) => res.render("editVideo");
+export const deleteVideo = (req, res) => res.render("deleteVideo");
