17da9e283a7c778ac30659a32c68708afca21f58
WHYjun <yjunchoi93@gmail.com>
2019-07-23 22:18:46 -0400

    update third-party login

--- middlewares.js
+++ middlewares.js
@@ -4,9 +4,9 @@ import routes from "./routes";
 const multerVideo = multer({ dest: "uploads/videos/" });
 
 export const localsMiddleware = (req, res, next) => {
-  res.locals.siteName = "Youtube Clone";
+  res.locals.siteName = "WeTube";
   res.locals.routes = routes;
-  res.locals.user = req.user || null;
+  res.locals.loggedUser = req.user || null;
   next();
 };
 
